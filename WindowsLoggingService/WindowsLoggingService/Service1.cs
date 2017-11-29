eef4cbc960243de90f5c10d7c3fff1142c629d09
Mateja Podravac <mateja.podravac@vsmti.hr>
2017-11-29 19:17:22 +0100

    dodan 8. zadatak

--- WindowsLoggingService/WindowsLoggingService/Service1.cs
+++ WindowsLoggingService/WindowsLoggingService/Service1.cs
@@ -49,16 +49,27 @@ namespace WindowsLoggingService
 
         public static void ScheduleService()
         {
-            // Objekt klase Timer
+            // Objekt klase Timer 
             Timer Schedular = new Timer(new TimerCallback(SchedularCallback));
+
             // Postavljanje vremena 'po defaultu'
             DateTime scheduledTime = DateTime.MinValue;
+
+            // !!!! dio koda (7. zad) je zakomentiran jer 8. zadatak zahtijeva mijenjanje tog dijela s drugim (do "Vremenski interval")
+            /*int intervalMinutes = 1;
+
+            // Postavljanje vremena zapisa u trenutno vrijeme + 1 minuta
+            scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+            if (DateTime.Now > scheduledTime)
+            {
+                scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+            }*/
+
             string mode = ConfigurationManager.AppSettings["Mode"].ToUpper();
             if (mode == "DAILY")
             {
                 //Dohvati vrijeme iz konfiguracijske datoteke.
-                scheduledTime =
-                DateTime.Parse(System.Configuration.ConfigurationManager.AppSettings["ScheduledTime"]);
+                scheduledTime = DateTime.Parse(System.Configuration.ConfigurationManager.AppSettings["ScheduledTime"]);
                 if (DateTime.Now > scheduledTime)
                 {
                     //Ukoliko je termin prošao, dodaj 1 dan.
@@ -68,8 +79,8 @@ namespace WindowsLoggingService
             if (mode.ToUpper() == "INTERVAL")
             {
                 // Dohvati vrijeme iz konfiguracijske datoteke
-                int intervalMinutes =
-                Convert.ToInt32(ConfigurationManager.AppSettings["IntervalMinutes"]);
+                int intervalMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalMinutes"]);
+
                 //Postavi zakazano vrijeme za jednu minutu od trenutnog vremena.
                 scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
                 if (DateTime.Now > scheduledTime)
@@ -78,12 +89,15 @@ namespace WindowsLoggingService
                     scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
                 }
             }
+
             // Vremenski interval
             TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
-            string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3}seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
             WriteToFile("Simple Service scheduled to run after: " + schedule + " {0}");
+
             //Razlika između trenutnog vremena i planiranog vremena
             int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
             // Promjena vremena izvršavanja metode povratnog poziva.
             Schedular.Change(dueTime, Timeout.Infinite);
         }
